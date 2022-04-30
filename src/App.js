0027731419d6906d20b4aef3a7512275f0bcd59e
fmgold <fmgold2015@gmail.com>
2022-04-30 13:03:47 +0100

    feat: pages setup with routerdom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./pages/home/home";
+import Login from "./pages/login/login";
+import List from "./pages/list/list";
+import Single from "./pages/single/Single";
+import New from "./pages/new/new";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={Home} />
+            <Route path="login" element={Login} />
+            <Route path="users">
+              <Route index element={List} />
+              <Route path=":userid" element={Single} />
+              <Route path="new" element={New} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import './home.scss'
+
+const home = () => {
+  return (
+    <div className='home'>
+    <h1 className='title'>Fmgold</h1>
+    </div>
+  )
+}
+
+export default home
